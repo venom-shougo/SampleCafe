0cc6fd2169cd4e28b0603232f556653ac0685a36
venom-shougo <wb.work.atelier@gmail.com>
2023-02-20 20:14:00 +0900

    データ更新

--- src/index.php
+++ src/index.php
@@ -1,5 +1,34 @@
 <?php
+phpinfo();
+// $to      = 'beat.0729.pp1@gmail.com';
+// $subject = 'Test email';
+// $message = 'This is a test email.';
+// $headers = 'From: your_username@gmail.com' . "\r\n" .
+//     'Reply-To: your_username@gmail.com' . "\r\n" .
+//     'X-Mailer: PHP/' . phpversion();
+
+// $smtp_server = 'smtp.gmail.com';
+// $smtp_port = 587;
+// $smtp_username = 'your_username@gmail.com';
+// $smtp_password = 'your_password';
+
+// //SMTPサーバーに接続するための設定を配列に格納
+// $smtp = array(
+//     'host' => $smtp_server,
+//     'port' => $smtp_port,
+//     'auth' => true,
+//     'username' => $smtp_username,
+//     'password' => $smtp_password
+// );
+
+// //メールを送信
+// if (mb_send_mail($to, $subject, $message, $headers, '-f' . $smtp_username)) {
+//     echo 'The email was sent successfully.';
+// } else {
+//     echo 'An error occurred while sending the email.';
+// }
 include('_header.php');
+
 ?>
 <body>
 
